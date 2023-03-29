fb0d8a42207bbdcaadc90a95bf955d9058bce74f
Neba Emmanuel <nebaemmanuel736@gmail.com>
2023-03-29 18:55:01 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './src/component/Button';
+import Input from './src/component/Input';
+import ListItem from './src/component/ListItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button/>
+      <Input/>
+      <ListItem/>
     </View>
   );
 }

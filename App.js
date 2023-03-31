07dd8af77280bd113c9b84af6e88e6dd70f7775f
Neba Emmanuel <nebaemmanuel736@gmail.com>
2023-03-31 07:38:52 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Button from './src/component/Button';
-import Input from './src/component/Input';
-import ListItem from './src/component/ListItem';
+import React, {useState} from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, FlatList, View, Keyboard, TouchableOpacity, ScrollView } from "react-native";
+import Button from "./src/component/Button";
+import Input from "./src/component/Input";
+import ListItem from "./src/component/ListItem";
+import Header from "./src/component/Header";
 
-export default function App() {
+const App = () => {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const deleteTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy)
+  }
+
+  const deleteAllTask = (index) => {
+    let deleteAll = [...taskItems];
+    deleteAll.slice(0, 0);
+    setTaskItems([])
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button/>
-      <Input/>
-      <ListItem/>
+      <View>
+        <Header style={{flex: 1}} onPress={() => deleteAllTask()}/>
+      </View>
+      <StatusBar style="auto" />  
+      
+        <View style={{flex: 4}}>
+          {
+            taskItems.map((item, index) => {
+              return (
+                
+                <View style={styles.listStyle}>
+                  <ListItem text={item} key={index}  onPress={() => deleteTask(index)}/> 
+                </View>
+                
+              )
+            })
+          }
+        </View>
+      
+      <View style={styles.bottomContainer}>
+        <Input value={task} onChangeText={text => setTask(text)} />
+        <Button onPress={() => handleAddTask()}/>
+      </View>
     </View>
   );
 }
@@ -19,8 +59,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    padding: 20,
   },
+  bottomContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: 'flex-end',
+    marginTop: 550,
+    flex: 2
+  },
+  listStyle:{
+    marginBottom: 15,
+    alignSelf: 'center'
+  }
+
 });
+
+export default App;
+
